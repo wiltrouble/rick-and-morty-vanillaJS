b3f763e24a8f01a5e056dfd9560e048cc148a08d
Wiltrouble <wiltrouble@gmail.com>
2021-02-12 23:45:00 -0400

    Design finished

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,48 @@
-console.log('hi wilson!!!')
+import API from './API.js';
+
+const api = new API();
+
+const characterContainer = document.querySelector('#character-container')
+// const characterTemplate = `
+//     <article class="character">
+//         <div class="character-grid">
+//             <h2>Rick sanches</h2>
+//             <img src="" alt="">
+//         </div>
+//     </article>
+// `
+
+export class Character {
+
+    constructor({name, image}) {
+        this.name = name;
+        this.image = image;
+        this.render();
+    }
+
+    build() {
+        return `
+        <article class="character">
+            <div class="character-grid">
+                <h2>${this.name} </h2>
+                <img src=${this.image} alt="">
+            </div>
+        </article>    
+        `   
+    }
+
+    render() {
+        characterContainer.innerHTML = this.build();
+    }
+
+}
+
+async function initApp(initCharacterId) {
+    const characterData = await api.getCharacter(initCharacterId)
+    console.log(characterData)
+    //const rick = new Character(characterData)
+}
+
+initApp(1)
+
+
