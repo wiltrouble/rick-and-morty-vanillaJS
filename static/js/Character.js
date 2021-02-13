f9facbc861155895c40865b675696583d59b653a
Wiltrouble <wiltrouble@gmail.com>
2021-02-13 00:35:23 -0400

    final changes

---
+++ static/js/Character.js
@@ -0,0 +1,50 @@
+export default class Character {
+
+    constructor({name, image, gender, species, status}) {
+        this.name = name;
+        this.image = image;
+        this.gender = gender;
+        this.species = species;
+        this.status = status
+        this.characterImageContainer = document.querySelector('#character-image-container');
+        this.characterDescriptionContainer = document.querySelector('#character-description-container');
+        this.characterNameContainer = document.querySelector('#character-name-container');
+        this.characterPlaceholder = document.querySelector('#character-name-placeholder');
+        this.render();
+    }
+
+    buildImage() {
+        return `
+            <img class="character-image" src=${this.image} alt=${this.name}> 
+        `   
+    }
+
+    buildName() {
+        return `
+            <div class="character-name">
+                <h2>${this.name}</h2>
+            </div>
+        `
+    }
+
+    buildDescription() {
+        return `
+            <div class="character-labels">
+                <h3 class="character-label">Gender: ${this.gender}</h3>
+                <h3 class="character-label">Species: ${this.species}</h3>
+                <h3 class="character-label">Status: ${this.status}</h3>
+            </div>
+        `
+    }
+
+    buildPlaceholder() {
+        return this.name;
+    }
+
+    render() {
+        this.characterImageContainer.innerHTML = this.buildImage();
+        this.characterNameContainer.innerHTML = this.buildName();
+        this.characterDescriptionContainer.innerHTML = this.buildDescription();
+        this.characterPlaceholder.innerHTML = this.buildPlaceholder();
+    }
+}
--- static/js/index.js
+++ static/js/index.js
@@ -1,48 +1,22 @@
 import API from './API.js';
+import Character from './Character.js'
 
 const api = new API();
+var currentCharacter = 1;
+const loadNext = document.querySelector('#load-next');
 
-const characterContainer = document.querySelector('#character-container')
-// const characterTemplate = `
-//     <article class="character">
-//         <div class="character-grid">
-//             <h2>Rick sanches</h2>
-//             <img src="" alt="">
-//         </div>
-//     </article>
-// `
+loadNext.addEventListener('click', async () => {
+    const characterData = await api.getCharacter(++currentCharacter)
+    new Character(characterData)
+})
 
-export class Character {
-
-    constructor({name, image}) {
-        this.name = name;
-        this.image = image;
-        this.render();
-    }
-
-    build() {
-        return `
-        <article class="character">
-            <div class="character-grid">
-                <h2>${this.name} </h2>
-                <img src=${this.image} alt="">
-            </div>
-        </article>    
-        `   
-    }
-
-    render() {
-        characterContainer.innerHTML = this.build();
-    }
-
-}
 
 async function initApp(initCharacterId) {
     const characterData = await api.getCharacter(initCharacterId)
     console.log(characterData)
-    //const rick = new Character(characterData)
+    new Character(characterData)
 }
 
-initApp(1)
+initApp(currentCharacter)
 
 
